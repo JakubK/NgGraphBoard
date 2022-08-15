9fd09bc15cdf326be0658b02ada0baa43893f1c2
Jakub Kuniszewski <you@example.com>
2022-08-15 22:02:43 +0200

    Add base Topbar and Board components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TopBarComponent } from './top-bar/top-bar.component';
+import { BoardComponent } from './board/board.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TopBarComponent,
+    BoardComponent
   ],
   imports: [
     BrowserModule,
