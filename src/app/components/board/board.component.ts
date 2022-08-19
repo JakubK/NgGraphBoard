ae5422e5746b8d84bfe443304b1d6915bd857c55
Jakub Kuniszewski <you@example.com>
2022-08-19 21:09:02 +0200

    Add styling for isolated nodes and hover on edges

--- src/app/components/board/board.component.ts
+++ src/app/components/board/board.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { map, Observable, startWith } from 'rxjs';
+import { map, Observable } from 'rxjs';
 import { BoardService } from '../../services/board.service';
 
 @Component({
