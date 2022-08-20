5920fd9f8e1b791205418a8fc52bb5721680abcc
Jakub Kuniszewski <you@example.com>
2022-08-20 13:49:36 +0200

    Add Board drag feature

--- src/app/components/board/board.component.ts
+++ src/app/components/board/board.component.ts
@@ -1,5 +1,7 @@
-import { Component } from '@angular/core';
-import { map, Observable } from 'rxjs';
+import { Component, ElementRef, OnInit } from '@angular/core';
+import { animationFrameScheduler,filter, fromEvent, scan, map, Observable, subscribeOn, switchMap, takeUntil, pairwise } from 'rxjs';
+import { CursorMode } from 'src/app/models/cursorMode';
+import { CursorService } from 'src/app/services/cursor.service';
 import { BoardService } from '../../services/board.service';
 
 @Component({
@@ -7,12 +9,52 @@ import { BoardService } from '../../services/board.service';
   templateUrl: './board.component.html',
   styleUrls: ['./board.component.scss']
 })
-export class BoardComponent {
+export class BoardComponent implements OnInit {
   zoom$: Observable<number>;
 
-  constructor(public readonly boardService: BoardService) {
+  translationX$!: Observable<number>;
+  translationY$!: Observable<number>;
+
+  constructor(public readonly boardService: BoardService,
+     private readonly cursorService: CursorService,
+     private readonly host: ElementRef) {
     this.zoom$ = boardService.zoom$.pipe(
       map(zoom => zoom / 100)
+    )
+  }
+
+  ngOnInit() {
+    const mouseMove$: Observable<MouseEvent> = fromEvent(this.host.nativeElement, 'mousemove');
+    const mouseUp$: Observable<MouseEvent> = fromEvent(this.host.nativeElement, 'mouseup');
+    const mouseDown$ = fromEvent(this.host.nativeElement, 'mousedown');
+
+    const move$ = (start: MouseEvent) => mouseMove$.pipe(
+      map((move) => ({left: move.clientX - start.offsetX, top: move.clientY - start.offsetY})),
+      takeUntil(mouseUp$)
+    )
+
+    const drag$ = mouseDown$.pipe(
+      switchMap(start => this.cursorService.activeCursorMode$$.pipe(
+        filter(mode => mode === CursorMode.MoveCamera),
+        switchMap(() => move$(start as MouseEvent)),
+        subscribeOn(animationFrameScheduler),
+        takeUntil(mouseUp$),
+        pairwise(),
+        map(([first, second]) => ({
+          left: first.left - second.left,
+          top: first.top - second.top,
+        }))
+      )),
+    );
+
+    this.translationX$ = drag$.pipe(
+      map((translation:{left: number; top: number;}) => translation.left),
+      scan((acc,curr) => acc - curr, 0)
+    )
+
+    this.translationY$ = drag$.pipe(
+      map((translation:{left: number; top: number;}) => translation.top),
+      scan((acc,curr) => acc - curr, 0)
     )
   }
 
